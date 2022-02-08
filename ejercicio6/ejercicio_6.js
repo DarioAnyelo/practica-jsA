44cc5504405a8bb4fda5fa64935bdaac50b4dcfd
Anyelo Dario <darioanyelo01@gmail.com>
2022-02-07 19:10:31 -0500

    respuesta a la pregunta 6

---
+++ ejercicio6/ejercicio_6.js
@@ -0,0 +1,30 @@
+const persona = {
+    name: 'ATahualpa',
+    anio: '1500',
+    inca: 'true',
+    number: 1,
+    hobies:[  'conquistar', 'guerrero'],
+    desendientes: {
+        primerhijo:{ name:'sinchiroka'},
+        segundohijo:{ name:'tupacyupanki'}
+    },
+    esposas:[ 'coya1', 'coya2']
+}
+
+const curso = {
+    asignatura : 'Algoritmo-1',
+    ciclo: 1,
+    obligatorio: 'true',
+    horario:['mañana','tarde'],
+    horas: 5,
+    docente:{ name:'falcon',edad:'40'}
+}
+
+const pais = {
+    name:'Perú',
+    lugar:'America del Sur',
+    idioma:['quechua','aimara','shipibo','español'],
+    oceano:'true',
+    departamento:25,
+    datoInterno:{ selva:'true', tamanio:'169000km'}   
+}
